01dea883431e6df2c79d67310eda84e14cedf33c
kajal.singh <kajalsingh1813@gmail.com>
2024-01-23 23:04:05 +0530

    Feat: Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import {Link} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href= "https://github.com/" target="_blank" >Github</a>
+      <h1>Kajal</h1>
     </div>
   );
 }
