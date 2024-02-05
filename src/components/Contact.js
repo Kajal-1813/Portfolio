327d89de2e6e4cdeda4a907fa49c4b036251ace3
kajal.singh <kajalsingh1813@gmail.com>
2024-02-05 08:14:46 +0530

    Feat : Added  Contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,14 @@
+import git from '../git.png'
+import linkin from '../linkin.png'
 const Contact = () => {
     return (
         <div className="contact">
-            <h3>8219837349</h3>
             <h3>kajalsingh1813@gmail.com</h3>
+            <div className="logos">
+            <a className="link" href = "http://www.linkedin.com/in/kajalsingh1813" target="_blank"><img className="logo" src = {linkin}></img></a>
+            <a  className="link" href = "https://github.com/Kajal-1813" target="_blank"><img className="logo" src = {git}></img></a>
+            </div>
+
         </div>
     )
 }
